27374cdd125cba5755225a7d409c46cde08f8622
Kanda Saikaew <krunapon@kku.ac.th>
2026-02-05 10:49:27 +0700

    add oop codes

---
+++ saikaew/kanda/oop/Test.java
@@ -0,0 +1,18 @@
+class Student {
+    private String name;
+    private double gpa;
+   
+    // Constructor with parameters
+    /*public Student(String name, double gpa) {
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    public Student() {} */
+}
+
+public class Test {
+    public static void main(String[] args) {
+        Student s = new Student();  // Line X
+    }
+}
